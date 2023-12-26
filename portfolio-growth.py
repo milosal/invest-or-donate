26a1cd04d0a462cab31b2fb8baa7f134cb1008f4
Cole Salvador <colesalvador@college.harvard.edu>
2023-12-26 01:54:25 -0600

    getting something up

--- portfolio-growth.py
+++ portfolio-growth.py
@@ -2,40 +2,24 @@
 I stipulate a portfolio of 25% NVDA, 25% GOOGL, 25% AMZN, 25% MSFT.
 '''
 import requests
-import datetime 
-import os
-from dotenv import load_dotenv
+import yfinance as yf
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-print(API_KEY)
+with open("key.txt", "r") as file:
+    API_KEY = file.read().strip()
+
 START_DATE = "2016-03-20"
 END_DATE = "2023-12-22"
-
-def convert_to_unix_time(date):
-    return int(datetime.datetime.strptime(date, "%Y-%m-%d").timestamp())
 
 data = {}
 
 # Portfolio contains...
 stocks = ["NVDA", "GOOGL", "AMZN", "MSFT"]
 
-# For each stock, get historical price 
 for stock in stocks:
+    current_ticker = yf.Ticker(stock)
+    print(current_ticker.history(period="1mo"))
+    data[stock] = []
 
-    params = {
-    "symbol": "NVDA,GOOGL,AMZN,MSFT",
-    "period1": START_DATE,
-    "period2": END_DATE,
-    "interval": "1d",
-    "events": "history",
-    }
+print(data)
 
-    response = requests.get("https://query1.finance.yahoo.com/v7/finance/download", params=params)
 
-    # Add to data dict if appropriate
-    if response.status_code == 200:
-        data[stock] = response.text
-        print(data[stock])
-    else:
-        print(f"Failed. Status code: {response.status_code}")
